a993368b1711821dc60f8bbf610dca3799f63330
Kristina Kapustina <kristinakapk@gmail.com>
2022-01-17 10:25:35 +0300

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,271 @@
+﻿
+namespace OKM
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series3 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.ButtonStart = new System.Windows.Forms.Button();
+            this.ButtonStop = new System.Windows.Forms.Button();
+            this.ButtonContinue = new System.Windows.Forms.Button();
+            this.ButtonReset = new System.Windows.Forms.Button();
+            this.Hight = new System.Windows.Forms.Label();
+            this.SpeedMax = new System.Windows.Forms.Label();
+            this.SpeedWind = new System.Windows.Forms.Label();
+            this.Angle = new System.Windows.Forms.Label();
+            this.Speed = new System.Windows.Forms.Label();
+            this.Dt = new System.Windows.Forms.Label();
+            this.textHight = new System.Windows.Forms.TextBox();
+            this.textSpeedMax = new System.Windows.Forms.TextBox();
+            this.textSpeedWind = new System.Windows.Forms.TextBox();
+            this.textAngle = new System.Windows.Forms.TextBox();
+            this.textSpeed = new System.Windows.Forms.TextBox();
+            this.textDt = new System.Windows.Forms.TextBox();
+            this.ChartTr = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            ((System.ComponentModel.ISupportInitialize)(this.ChartTr)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // ButtonStart
+            // 
+            this.ButtonStart.Location = new System.Drawing.Point(12, 348);
+            this.ButtonStart.Name = "ButtonStart";
+            this.ButtonStart.Size = new System.Drawing.Size(75, 23);
+            this.ButtonStart.TabIndex = 0;
+            this.ButtonStart.Text = "Пуск";
+            this.ButtonStart.UseVisualStyleBackColor = true;
+            this.ButtonStart.Click += new System.EventHandler(this.ButtonStart_Click);
+            // 
+            // ButtonStop
+            // 
+            this.ButtonStop.Location = new System.Drawing.Point(93, 348);
+            this.ButtonStop.Name = "ButtonStop";
+            this.ButtonStop.Size = new System.Drawing.Size(75, 23);
+            this.ButtonStop.TabIndex = 1;
+            this.ButtonStop.Text = "Стоп";
+            this.ButtonStop.UseVisualStyleBackColor = true;
+            this.ButtonStop.Click += new System.EventHandler(this.ButtonStop_Click);
+            // 
+            // ButtonContinue
+            // 
+            this.ButtonContinue.Location = new System.Drawing.Point(174, 348);
+            this.ButtonContinue.Name = "ButtonContinue";
+            this.ButtonContinue.Size = new System.Drawing.Size(75, 23);
+            this.ButtonContinue.TabIndex = 2;
+            this.ButtonContinue.Text = "Продолжить";
+            this.ButtonContinue.UseVisualStyleBackColor = true;
+            this.ButtonContinue.Click += new System.EventHandler(this.ButtonContinue_Click);
+            // 
+            // ButtonReset
+            // 
+            this.ButtonReset.Location = new System.Drawing.Point(255, 348);
+            this.ButtonReset.Name = "ButtonReset";
+            this.ButtonReset.Size = new System.Drawing.Size(75, 23);
+            this.ButtonReset.TabIndex = 3;
+            this.ButtonReset.Text = "Сброс";
+            this.ButtonReset.UseVisualStyleBackColor = true;
+            this.ButtonReset.Click += new System.EventHandler(this.ButtonReset_Click);
+            // 
+            // Hight
+            // 
+            this.Hight.AutoSize = true;
+            this.Hight.Location = new System.Drawing.Point(12, 30);
+            this.Hight.Name = "Hight";
+            this.Hight.Size = new System.Drawing.Size(102, 13);
+            this.Hight.TabIndex = 4;
+            this.Hight.Text = "Начальная высота";
+            // 
+            // SpeedMax
+            // 
+            this.SpeedMax.AutoSize = true;
+            this.SpeedMax.Location = new System.Drawing.Point(12, 56);
+            this.SpeedMax.Name = "SpeedMax";
+            this.SpeedMax.Size = new System.Drawing.Size(134, 13);
+            this.SpeedMax.TabIndex = 5;
+            this.SpeedMax.Text = "Максимальная скорость";
+            // 
+            // SpeedWind
+            // 
+            this.SpeedWind.AutoSize = true;
+            this.SpeedWind.Location = new System.Drawing.Point(12, 84);
+            this.SpeedWind.Name = "SpeedWind";
+            this.SpeedWind.Size = new System.Drawing.Size(87, 13);
+            this.SpeedWind.TabIndex = 6;
+            this.SpeedWind.Text = "Скорость ветра";
+            // 
+            // Angle
+            // 
+            this.Angle.AutoSize = true;
+            this.Angle.Location = new System.Drawing.Point(12, 108);
+            this.Angle.Name = "Angle";
+            this.Angle.Size = new System.Drawing.Size(72, 13);
+            this.Angle.TabIndex = 7;
+            this.Angle.Text = "Угол вылета";
+            // 
+            // Speed
+            // 
+            this.Speed.AutoSize = true;
+            this.Speed.Location = new System.Drawing.Point(12, 138);
+            this.Speed.Name = "Speed";
+            this.Speed.Size = new System.Drawing.Size(112, 13);
+            this.Speed.TabIndex = 8;
+            this.Speed.Text = "Начальная скорость";
+            // 
+            // Dt
+            // 
+            this.Dt.AutoSize = true;
+            this.Dt.Location = new System.Drawing.Point(12, 165);
+            this.Dt.Name = "Dt";
+            this.Dt.Size = new System.Drawing.Size(112, 13);
+            this.Dt.TabIndex = 9;
+            this.Dt.Text = "Шаг интегрирования";
+            // 
+            // textHight
+            // 
+            this.textHight.Location = new System.Drawing.Point(137, 30);
+            this.textHight.Name = "textHight";
+            this.textHight.Size = new System.Drawing.Size(100, 20);
+            this.textHight.TabIndex = 10;
+            // 
+            // textSpeedMax
+            // 
+            this.textSpeedMax.Location = new System.Drawing.Point(137, 56);
+            this.textSpeedMax.Name = "textSpeedMax";
+            this.textSpeedMax.Size = new System.Drawing.Size(100, 20);
+            this.textSpeedMax.TabIndex = 11;
+            // 
+            // textSpeedWind
+            // 
+            this.textSpeedWind.Location = new System.Drawing.Point(137, 84);
+            this.textSpeedWind.Name = "textSpeedWind";
+            this.textSpeedWind.Size = new System.Drawing.Size(100, 20);
+            this.textSpeedWind.TabIndex = 12;
+            // 
+            // textAngle
+            // 
+            this.textAngle.Location = new System.Drawing.Point(137, 110);
+            this.textAngle.Name = "textAngle";
+            this.textAngle.Size = new System.Drawing.Size(100, 20);
+            this.textAngle.TabIndex = 13;
+            // 
+            // textSpeed
+            // 
+            this.textSpeed.Location = new System.Drawing.Point(137, 138);
+            this.textSpeed.Name = "textSpeed";
+            this.textSpeed.Size = new System.Drawing.Size(100, 20);
+            this.textSpeed.TabIndex = 14;
+            // 
+            // textDt
+            // 
+            this.textDt.Location = new System.Drawing.Point(137, 165);
+            this.textDt.Name = "textDt";
+            this.textDt.Size = new System.Drawing.Size(100, 20);
+            this.textDt.TabIndex = 15;
+            // 
+            // ChartTr
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.ChartTr.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.ChartTr.Legends.Add(legend1);
+            this.ChartTr.Location = new System.Drawing.Point(313, 12);
+            this.ChartTr.Name = "ChartTr";
+            series1.ChartArea = "ChartArea1";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+            series1.Legend = "Legend1";
+            series1.Name = "Высота";
+            series2.ChartArea = "ChartArea1";
+            series2.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+            series2.Legend = "Legend1";
+            series2.Name = "Дальность";
+            series3.ChartArea = "ChartArea1";
+            series3.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+            series3.Legend = "Legend1";
+            series3.Name = "Скорость";
+            this.ChartTr.Series.Add(series1);
+            this.ChartTr.Series.Add(series2);
+            this.ChartTr.Series.Add(series3);
+            this.ChartTr.Size = new System.Drawing.Size(422, 300);
+            this.ChartTr.TabIndex = 1;
+            this.ChartTr.Text = "ChartTr";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.ChartTr);
+            this.Controls.Add(this.textDt);
+            this.Controls.Add(this.textSpeed);
+            this.Controls.Add(this.textAngle);
+            this.Controls.Add(this.textSpeedWind);
+            this.Controls.Add(this.textSpeedMax);
+            this.Controls.Add(this.textHight);
+            this.Controls.Add(this.Dt);
+            this.Controls.Add(this.Speed);
+            this.Controls.Add(this.Angle);
+            this.Controls.Add(this.SpeedWind);
+            this.Controls.Add(this.SpeedMax);
+            this.Controls.Add(this.Hight);
+            this.Controls.Add(this.ButtonReset);
+            this.Controls.Add(this.ButtonContinue);
+            this.Controls.Add(this.ButtonStop);
+            this.Controls.Add(this.ButtonStart);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.ChartTr)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button ButtonStart;
+        private System.Windows.Forms.Button ButtonStop;
+        private System.Windows.Forms.Button ButtonContinue;
+        private System.Windows.Forms.Button ButtonReset;
+        private System.Windows.Forms.Label Hight;
+        private System.Windows.Forms.Label SpeedMax;
+        private System.Windows.Forms.Label SpeedWind;
+        private System.Windows.Forms.Label Angle;
+        private System.Windows.Forms.Label Speed;
+        private System.Windows.Forms.Label Dt;
+        private System.Windows.Forms.TextBox textHight;
+        private System.Windows.Forms.TextBox textSpeedMax;
+        private System.Windows.Forms.TextBox textSpeedWind;
+        private System.Windows.Forms.TextBox textAngle;
+        private System.Windows.Forms.TextBox textSpeed;
+        private System.Windows.Forms.TextBox textDt;
+        private System.Windows.Forms.DataVisualization.Charting.Chart ChartTr;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,260 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace OKM
+{
+    public partial class Form1 : Form
+    {
+        private double hight;
+        private double speedMax;
+        private double dt;
+        private vect speedWind;
+        private vect speed;
+        private vect r;
+        private const int n = 10000;
+        private vect[] Tr = new vect[n];
+        private double[] Vt = new double[n];
+        private int iter = 0;
+        private int i1 = 0;
+        private vect gravity;
+        public Form1()
+        {
+            InitializeComponent();
+            textHight.Text = "30";
+            textAngle.Text = "45";
+            textSpeedMax.Text = "120";
+            textSpeed.Text = "60";
+            textSpeedWind.Text = "0";
+            textDt.Text = "0,1";
+        }
+
+
+
+        private System.Windows.Forms.Timer MainTimer;
+        private System.Windows.Forms.Timer ChartTimer;
+
+        private void ButtonStart_Click(object sender, EventArgs e)
+        {
+            if (!ValidateInput()) return;
+            SetTimers();
+            ChartTimer.Start();
+            ButtonStart.Enabled = false;
+        }
+
+        private void ButtonStop_Click(object sender, EventArgs e)
+        {
+            MainTimer.Stop();
+            ChartTimer.Stop();
+        }
+
+        private void ButtonContinue_Click(object sender, EventArgs e)
+        {
+            MainTimer.Stop();
+            ChartTimer.Stop();
+        }
+        private void Verle(Object source, EventArgs e)
+        {
+            iter++;
+            if (hight <= 0) return;
+            vect acceleration = GetAcceleration(speed);
+            r += dt * speed + 0.5 * dt * dt * acceleration;
+            hight = r.y;
+
+            vect predictor = speed + dt * acceleration;
+
+            speed += 0.5 * dt * acceleration;
+            acceleration = GetAcceleration(predictor);
+            speed += 0.5 * dt * acceleration;
+
+            Tr[iter] = r;
+            Vt[iter] = speed.Mod;
+            if (hight < 0) Reset();
+        }
+        private void Reset()
+        {
+            MainTimer.Stop();
+            iter = 0;
+            ButtonStart.Enabled = true;
+        }
+        private void ChartReset()
+        {
+            ChartTr.Series[0].Points.Clear();
+            ChartTr.Series[1].Points.Clear();
+            ChartTr.Series[0].Points.Clear();
+        }
+        private vect GetAcceleration(vect speed)
+        {
+            vect velocity = speed - speedWind;
+            return gravity - Math.Exp(-hight / 10000) * gravity.Mod * velocity.Mod / (speedMax * speedMax) * velocity;
+        }
+        private bool ValidateInput()
+        {
+            bool res = true;
+
+            double hight;
+            bool check = System.Double.TryParse(textHight.Text, out hight);
+            if (!check || hight < 0)
+            {
+                MessageBox.Show("Некорректна начальная высота");
+                res = false;
+            }
+
+            double angle;
+            check = System.Double.TryParse(textAngle.Text, out angle);
+            if (!check || !(angle > 0 && angle <= 90))
+            {
+                MessageBox.Show("Некорректен угол");
+                res = false;
+            }
+            double speedMax;
+            check = System.Double.TryParse(textSpeedMax.Text, out speedMax);
+            if (!check || speedMax < 0)
+            {
+                MessageBox.Show("Некорректна максимальная скорость");
+                res = false;
+            }
+            double speed;
+            check = System.Double.TryParse(textSpeed.Text, out speed);
+            if (!check || speed < 0)
+            {
+                MessageBox.Show("Некорректна начальная скорость");
+                res = false;
+            }
+            double speedWind;
+            check = System.Double.TryParse(textSpeedWind.Text, out speedWind);
+            if (!check)
+            {
+                MessageBox.Show("Некорректна начальная скорость");
+                res = false;
+            }
+            double dt;
+            check = System.Double.TryParse(textDt.Text, out dt);
+            if (!check || dt <= 0)
+            {
+                MessageBox.Show("Некорректен шаг интегрирования");
+                res = false;
+            }
+            if (res)
+            {
+                this.hight = hight;
+                this.dt = dt;
+                this.speedMax = speedMax;
+                this.speedWind = new vect(speedWind, 0, 0);
+                this.speed = new vect(speed * Math.Cos(angle), speed * Math.Sin(angle), 0);
+                this.r = new vect(0, hight, 0);
+                this.gravity = new vect(0, -9.81, 0);
+                Tr[0] = new vect(0, hight, 0);
+                Vt[0] = speed;
+            }
+            return res;
+        }
+        private void Visualize(object source, EventArgs e)
+        {
+            for (int i = i1; i < iter; i++)
+            {
+                if (hight < 0)
+                {
+                    ChartTimer.Stop();
+                    i1 = 0;
+                    return;
+                }
+
+             ChartTr.Series[2].Points.AddXY(i + dt, Vt[i]);
+             ChartTr.Series[1].Points.AddXY(i + dt, Tr[i].x);
+             ChartTr.Series[0].Points.AddXY(i + dt, Tr[i].y);
+            }
+            i1 = iter;
+        }
+        private void SetTimers()
+        {
+            MainTimer = new System.Windows.Forms.Timer();
+            MainTimer.Tick += new EventHandler(Verle);
+
+            MainTimer.Start();
+
+            ChartTimer = new System.Windows.Forms.Timer();
+            ChartTimer.Tick += new EventHandler(Visualize);
+            ChartTimer.Start();
+        }
+        private void ButtonReset_Click(object sender, EventArgs e)
+        {
+            textHight.Text = "";
+            textAngle.Text = "";
+            textSpeedMax.Text = "";
+            textSpeed.Text = "";
+            textSpeedWind.Text = "";
+            textDt.Text = "";
+            Reset();
+        }
+    }
+}
+
+
+
+public struct vect
+{
+    public double x, y, z;
+    public double modul()
+    {
+        return Math.Sqrt(x * x + y * y + z * z);
+    }
+    public double Mod
+    {
+        get { return Math.Sqrt(x * x + y * y + z * z); }
+    }
+    public vect(double x, double y, double z)
+    {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+    public static vect operator +(vect a, vect b)
+    {
+        vect c = new vect();
+        c.x = a.x + b.x;
+        c.y = a.y + b.y;
+        return c;
+    }
+    public static double operator *(vect a, vect b)
+    {
+        return a.x * b.x + a.y * b.y;
+    }
+    public static vect operator *(double c, vect a)
+    {
+        vect h = new vect();
+        h.x = a.x * c;
+        h.y = a.y * c;
+        return h;
+    }
+    public static vect operator *(vect a, double c)
+    {
+        vect h = new vect();
+        h.x = a.x * c;
+        h.y = a.y * c;
+        return h;
+    }
+    public static vect operator -(vect a, vect b)
+    {
+        vect c = new vect();
+        c.x = a.x - b.x;
+        c.y = a.y - b.y;
+        return c;
+    }
+    public static vect operator &(vect a, vect b)
+    {
+        vect c = new vect();
+        c.x = a.y * b.z - a.z * b.y;
+        c.x = a.z * b.y - a.y * b.z;
+        c.x = a.x * b.z - a.z * b.x;
+        return c;
+    }
+
+}
+
